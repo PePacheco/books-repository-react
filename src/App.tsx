4c24ab0ed604eb1253f30816444f2fd0b47b970a
PePacheco <pedro_pacheco98@outlook.com>
2023-05-04 16:30:17 -0300

    Pluged the UI to the fake API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BookCreate } from './components/BookCreate'
 import { BookList } from './components/BookList'
 
@@ -12,24 +12,59 @@ export type Book = {
 function App() {
     const [books, setBooks] = useState<Book[]>([])
 
-    const handleCreateBook = (book: Book) => {
-        setBooks(previosBooks => [...previosBooks, book])
+    const fetchBooks = async () => {
+        const response = await fetch('http://localhost:3001/books')
+        const books = await response.json()
+        setBooks(books)
     }
 
-    const handleDeleteBook = (id: string) => {
-        const newBooks = books.filter(book => book.id !== id)
-        setBooks(newBooks)
+    const handleCreateBook = async (book: Book) => {
+        const response = await fetch('http://localhost:3001/books', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(book),
+        })
+        const createdBook = await response.json()
+
+        setBooks(previosBooks => [...previosBooks, createdBook])
     }
 
-    const handleEditBook = (id: string, title: string) => {
+    const handleDeleteBook = async (id: string) => {
+        const response = await fetch(`http://localhost:3001/books/${id}`, {
+            method: 'DELETE'
+        })
+
+        if (response.ok) {
+            const newBooks = books.filter(book => book.id !== id)
+            setBooks(newBooks)
+        }
+    }
+
+    const handleEditBook = async (id: string, title: string) => {
+        const response = await fetch(`http://localhost:3001/books/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title }),
+        })
+
+        const updatedBook = await response.json()
+
         const updatedBooks: Book[] = books.map(book => {
-            if (book.id === id) {
-                return { ...book, title }
+            if (updatedBook.id === id) {
+                return { ...book, ...updatedBook }
             }
             return book
         })
         setBooks(updatedBooks)
     }
+
+    useEffect(() => {
+        fetchBooks()
+    }, [])
 
     return (
         <>
